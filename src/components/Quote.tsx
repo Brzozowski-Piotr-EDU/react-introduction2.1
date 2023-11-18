06dfb63b6cb004e1b83368eb6bba21777745bbf7
Brzozowski-Piotr-EDU <piobrzedu@icloud.com>
2023-11-18 23:40:39 +0100

    Added nearly proper styling

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -6,17 +6,20 @@ export interface QuoteProps {
 
 export const QuoteComponent = ({ quote }: QuoteProps) => {
   return (
-    <div>
-      <div className="img">
+    <div className="quote-container">
+      <div className="imgDiv">
         <a href={`https://pl.wikipedia.org/wiki/${quote.author}`}>
           <img
             src={require(`../assets/images/authors/${quote.author}.jpg`)}
             alt="Quote author"
           ></img>
         </a>
+        <p>Click image for more informations!</p>
       </div>
-      <p className="quote__text">"{quote.text}"</p>
-      <p className="quote__author">- {quote.author}</p>
+      <div className="quote">
+        <p className="quote__text">"{quote.text}"</p>
+        <p className="quote__author">- {quote.author}</p>
+      </div>
     </div>
   );
 };
